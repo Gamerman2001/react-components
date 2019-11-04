6496d4686beb5a21f022c0c25b55acc8347115fe
Gamerman2001 <Tro12185@gmail.com>
2019-11-04 15:53:24 -0500

    added js files

---
+++ src/Components/PortfolioBody.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './PortfolioBody.css'
+
+export default function PortfolioBody() {
+  return (
+    <div className="port-body">
+      Hello from Body
+    </div>
+  )
+}
