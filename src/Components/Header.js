7852053c9b29724ae3e5cbf5670fd20be6ef3f36
Gamerman2001 <Tro12185@gmail.com>
2019-11-04 15:58:28 -0500

    header items added

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,7 +4,16 @@ import './Header.css'
 export default function Header() {
   return (
     <div className="header-body">
-      
+      <div className="header-name">
+        Troy Gooden
+      </div>
+
+      <div className="header-topics">
+        Home 
+        Portfolio
+        About Me 
+        Contact Me
+      </div>
     </div>
   )
 }
