8e83fc90b227a0f65e638a4a1b17d56c82a3da1d
Gamerman2001 <Tro12185@gmail.com>
2019-11-17 17:23:00 -0500

    added social media icons, need to add links

--- src/Components/PortfolioBody.js
+++ src/Components/PortfolioBody.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Button, Icon } from 'antd'
 import './PortfolioBody.css'
 
 export default function PortfolioBody() {
@@ -7,8 +8,16 @@ export default function PortfolioBody() {
       <div className="word-wrap">
         I am a DEVELOPER
       </div>
+      <div className="projects">
+
+      </div>
       <div className="social-media">
-        
+        <Button type='primary'>hi</Button>
+        <Icon type="github" theme='filled'height='6em' width='4em'/>
+        <Icon type="linkedin" />
+        <Icon type="medium" />
+        <Icon type="twitter" />
+
       </div>
     </div>
   )
