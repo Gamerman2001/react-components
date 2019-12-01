5d86d29e34fe814b3f232d586f1c74d963ea9da0
Gamerman2001 <Tro12185@gmail.com>
2019-12-01 13:00:36 -0500

    added other social media links

--- src/Components/SocialMedia.js
+++ src/Components/SocialMedia.js
@@ -7,9 +7,8 @@ export default function SocialMedia() {
   return (
     <div className="social-media">
         <a href="https://github.com/Gamerman2001"><Icon type="github"  /></a>
-        <a href=""><Icon type="linkedin" />  </a>
-        <a href=""><Icon type="medium" /></a>
-        <a href=""><Icon type="twitter" /></a> 
+        <a href="https://www.linkedin.com/in/troygooden/"><Icon type="linkedin" />  </a>
+        <a href="https://medium.com/@tro12185"><Icon type="medium" /></a>
       </div>
   )
 }
