05dba38795839be4ce0c28d89c044fe53f41d020
Gamerman2001 <Tro12185@gmail.com>
2019-12-07 03:20:29 -0500

    all links work to control menu properly

--- src/Components/MenuPage.js
+++ src/Components/MenuPage.js
@@ -2,22 +2,28 @@ import React from "react";
 import "./MenuPage.css";
 import { Link } from "react-router-dom";
 
-export default function MenuPage({setMenu}) {
+export default function MenuPage({ setMenu }) {
   return (
     <div className="menu-page">
       {console.log(setMenu)}
       <div className="top-background">
         <div className="menu-items">
-          <Link to='/' onClick={()=> setMenu()}>Home</Link>
-          <Link to='projects'>Projects </Link>
-          <Link to='about'>About </Link>
-          <Link to='contact'>Contact Me</Link>
-          </div>
+          <Link to="/" onClick={() => setMenu()}>
+            Home
+          </Link>
+          <Link to="projects" onClick={() => setMenu()}>
+            Projects{" "}
+          </Link>
+          <Link to="about" onClick={() => setMenu()}>
+            About{" "}
+          </Link>
+          <Link to="contact" onClick={() => setMenu()}>
+            Contact Me
+          </Link>
+        </div>
       </div>
       <div className="bottom-background">
-        <div className="initials">
-          TG
-        </div>
+        <div className="initials">TG</div>
       </div>
     </div>
   );
