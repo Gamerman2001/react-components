5c4a131a89ee96ff89cb15b66c6ed667689e5e1d
Gamerman2001 <Tro12185@gmail.com>
2019-12-11 14:11:12 -0500

    added about me section text

--- src/Components/AboutMe.js
+++ src/Components/AboutMe.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import './AboutMe.css'
 
 export default function AboutMe() {
   return (
     <div className='about-me'>
-      about me
+      <h1 className='about-me__heading'>So a little about me? </h1>
+      
+      <p className='about-me__body'>I have an eagerness to learn and excel in absorbing new technologies and successfully applying them to develop clean and well-structured code. One problem we face in this industry is assuming that all problems can be solved by code. Hiring me for your projects increases your chances of having someone who values not just code, but real people. I have experience using languages & technologies such as React, Ruby, PostgresSQL, JavaScript, XML, HTML, CSS, Express and Redux to name a few. I have a strong ability to build excellent relationships with clients and team members and always bring positive energy to any industry who needs people who can consistently be engaged and motivate others.</p>
     </div>
   )
 }
