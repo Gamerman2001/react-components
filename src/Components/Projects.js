25f2433fbd37604348924323dba7feded10af3ec
Gamerman2001 <Tro12185@gmail.com>
2019-12-22 23:25:46 -0500

    adding grid template for about me page

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -8,9 +8,9 @@ export default function Projects() {
 
   return (
     <div className='projects'>
-      <div className="projects__project1">React-Resort-Demo React-Resort-github</div>
-      <div className="projects__project2">RPS Hero testing this too</div>
-      <div className="projects__project3">Affirm Action</div>
+      <div className="projects__project1">React-Resort-Demo <h2>React-Resort-github<Icon type="github"  /></h2></div>
+      <div className="projects__project2">RPS Hero testing this too<Icon type="github"  /></div>
+      <div className="projects__project3">Affirm Action<Icon type="github"  /></div>
     </div>
   )
 }
