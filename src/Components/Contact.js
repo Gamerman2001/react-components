c3644f02142a395007a2abcd997aaf8e3c0f6132
Gamerman2001 <Tro12185@gmail.com>
2019-12-29 16:27:32 -0500

    form submit captures value, needs submit function

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -8,13 +8,14 @@ export default function Contact() {
   const [form, setForm] = useState('');
   const handleChange = (event) => {
     setForm(event.target.value);
+    console.log(event.target.value)
   }
   return (
     <div className="contact">
       <form>
         <label>
           Name:
-          <input type="text" name="name" />
+          <input type="text" value={form} onChange={handleChange} name="name" />
         </label>
         <input type="submit" value="Submit" />
       </form>
