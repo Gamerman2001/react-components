1fd805c79415c02165d717774c23be55f84cbc95
Gamerman2001 <Tro12185@gmail.com>
2020-01-04 20:44:33 -0500

    added more icons

--- src/Components/Intro.js
+++ src/Components/Intro.js
@@ -11,6 +11,8 @@ export default function Intro() {
       <img alt="css3" src="https://img.icons8.com/color/48/000000/css3.png"></img>
       <img alt="html5" src="https://img.icons8.com/color/48/000000/html-5.png"></img>
       <img alt="js" src="https://img.icons8.com/color/48/000000/javascript.png"></img>
+      <img alt="react" src="https://img.icons8.com/color/48/000000/react-native.png"></img>
+      <img alt="node" src="https://img.icons8.com/color/48/000000/npm.png"></img>
       <img alt="ruby" src="https://img.icons8.com/color/48/000000/ruby-programming-language.png"></img>
     </div>
     
