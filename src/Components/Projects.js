98e3a55cd5ab5eef50ca394d8e0da8afbcc210c5
Gamerman2001 <Tro12185@gmail.com>
2020-01-05 01:02:52 -0500

    added background images

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -8,9 +8,10 @@ export default function Projects() {
 
   return (
     <div className='projects'>
-      <div className="projects__project1">React-Resort-Demo <h2>React-Resort-github<Icon type="github"  /></h2></div>
+      <div className="projects__project1">React Resort <h2>React-Resort-github<Icon type="github"  /></h2></div>
       <div className="projects__project2">RPS Hero testing this too<Icon type="github"  /></div>
-      <div className="projects__project3">Affirm Action<Icon type="github"  /></div>
+      <div className="projects__project3">Affirm Action<Icon type="github"  />
+      <h3>testing this too</h3></div>
     </div>
   )
 }
